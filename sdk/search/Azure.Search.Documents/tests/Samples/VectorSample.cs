2e6dfd26018d6104beb87152ada04527b27c88e6
ShivangiReja <shivangi.reja@microsoft.com>
2024-04-01 18:34:19 -0700

    Test

--- sdk/search/Azure.Search.Documents/tests/Samples/VectorSample.cs
+++ sdk/search/Azure.Search.Documents/tests/Samples/VectorSample.cs
@@ -24,36 +24,19 @@ namespace Azure.Search.Documents.Tests.Samples.VectorSearch
         {
             public string Id { get; set; }
             public string Description { get; set; }
-            public ReadOnlyMemory<float> DescriptionVector { get; set; }
         }
 
         [Test]
-        public async Task MergeOrUploadVectorTest()
+        public async Task MergeOrUploadTest()
         {
-            string vectorSearchProfileName = "my-vector-profile";
-            string vectorSearchHnswConfig = "my-hsnw-vector-config";
-            int modelDimensions = 1536;
-
-            string indexName = "myindex";
+            string indexName = "mysearchindex";
             SearchIndex searchIndex = new(indexName)
             {
                 Fields =
                 {
                     new SimpleField("Id", SearchFieldDataType.String) { IsKey = true, IsFilterable = true, IsSortable = true, IsFacetable = true },
                     new SearchableField("Description") { IsFilterable = true },
-                    new VectorSearchField("DescriptionVector", modelDimensions, vectorSearchProfileName),
-                },
-                VectorSearch = new()
-                {
-                    Profiles =
-                    {
-                        new VectorSearchProfile(vectorSearchProfileName, vectorSearchHnswConfig)
-                    },
-                    Algorithms =
-                    {
-                        new HnswAlgorithmConfiguration(vectorSearchHnswConfig)
-                    }
-                },
+                }
             };
 
             Uri endpoint = new(Environment.GetEnvironmentVariable("SEARCH_ENDPOINT"));
@@ -70,30 +53,15 @@ namespace Azure.Search.Documents.Tests.Samples.VectorSearch
                         "Best hotel in town if you like luxury hotels. They have an amazing infinity pool, a spa, " +
                         "and a really helpful concierge. The location is perfect -- right downtown, close to all " +
                         "the tourist attractions. We highly recommend this hotel.",
-                DescriptionVector = GetEmbeddings(
-                        "Best hotel in town if you like luxury hotels. They have an amazing infinity pool, a spa, " +
-                        "and a really helpful concierge. The location is perfect -- right downtown, close to all " +
-                        "the tourist attractions. We highly recommend this hotel."),
             };
 
             SearchClient searchClient = new(endpoint, indexName, credential);
-            Response<IndexDocumentsResult> result = await searchClient.MergeOrUploadDocumentsAsync(new[] { document });
+
+            var batch = IndexDocumentsBatch.MergeOrUpload(new[] { document });
+            Response<IndexDocumentsResult> result = await searchClient.IndexDocumentsAsync(batch);
 
             Assert.AreEqual(200, result.GetRawResponse().Status);
             Assert.AreEqual("1", result.Value.Results[0].Key);
-        }
-
-        public static ReadOnlyMemory<float> GetEmbeddings(string input)
-        {
-            Uri endpoint = new Uri(Environment.GetEnvironmentVariable("OpenAI_ENDPOINT"));
-            string key = Environment.GetEnvironmentVariable("OpenAI_API_KEY");
-            AzureKeyCredential credential = new AzureKeyCredential(key);
-
-            OpenAIClient openAIClient = new OpenAIClient(endpoint, credential);
-            EmbeddingsOptions embeddingsOptions = new("text-embedding-ada-002", new string[] { input });
-
-            Embeddings embeddings = openAIClient.GetEmbeddings(embeddingsOptions);
-            return embeddings.Data[0].Embedding;
         }
     }
 }
